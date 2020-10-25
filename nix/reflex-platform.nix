cb69b368beffe967ae1b6d6deab0ab7269a5d915
Ashesh Ambasta <ashesh.ambasta@gmail.com>
2020-10-25 17:27:39 +0100

    Reflex platform version.

--- nix/reflex-platform.nix
+++ nix/reflex-platform.nix
@@ -1,7 +1,7 @@
 let
-  rev = "df0bdcca5eb2a3236ec0496e4430d91876b29cf5";
+  rev = "b14992ae74cac755f0e972569434c71ad35489eb";
 in import (builtins.fetchTarball
   {
     url = "https://github.com/reflex-frp/reflex-platform/archive/${rev}.tar.gz";
-    sha256 = "1ja3vkq9px8f9iyiazq44mamaahgiphi9l236pvzbl5jvhi5c4qr";
+    sha256 = "0znbjnzmv00mjyl2w8lpw10v9qmi5dffhzm8jqm6ypgil5xfylx3";
   }) {}
